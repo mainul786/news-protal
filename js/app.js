3ce98d548068a21269a850ad7e5822bc4f8f366e
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-09-14 22:24:50 +0530

    data categories data load

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('news protal connected')
+const loadCategories = async() => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data.data)
+    } 
+    catch(error){
+        console.log(error)
+    }
+}
+
+
+loadCategories();
