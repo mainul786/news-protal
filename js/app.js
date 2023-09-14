2c91c4733c88a5ffc46bea7abb5f3714c975f633
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-09-14 23:15:18 +0530

    show news length catagory wise

--- js/app.js
+++ js/app.js
@@ -11,18 +11,42 @@ const loadCategories = async() => {
 }
 
 const dispalyCategories = categories =>{
-    console.log(categories);
+    // console.log(categories);
     const categoriesContainer = document.getElementById('categories');
     categories.forEach(category =>{
-        console.log(category);
+        // console.log(category);
         const div = document.createElement('div');
         div.classList.add('col');
         div.style.fontSize = '14px';
         div.innerHTML = `
-        <span>${category.category_name}</span>
+        <span onclick="categoriesData('${category.category_id}')">${category.category_name}</span>
         `;
         categoriesContainer.appendChild(div);
     })
 }
+
+const categoriesData = async (categoriesId) =>{
+    // console.log(categoriesId);
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoriesId}`;
+   try{
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsData(data.data);
+    // console.log(data.data);
+   }
+   catch(error){
+    console.log(error);
+   }
+}
+
+const displayNewsData = async (newsData) =>{
+console.log(newsData);
+const newsHeading = document.getElementById('news-heading');
+newsHeading.innerText=`${newsData.length}  items found for category `;
+console.log(newsData.length)
+newsData.forEach(news => {
+    console.log(news); 
+});
+};
 
 loadCategories();
